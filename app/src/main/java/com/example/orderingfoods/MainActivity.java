57c7823d8b837673f9cbf7083f3571651dbaa028
orgin <https://github.com/nguyentoannn/OrderFoods>
2022-10-05 16:17:27 +0700

    resolve conflict

--- app/src/main/java/com/example/orderingfoods/MainActivity.java
+++ app/src/main/java/com/example/orderingfoods/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.orderingfoods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView userInformation, homePage, settings, cart, favorite;
+    Button _toLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _toLogin = findViewById(R.id.home_login);
+        userInformation = findViewById(R.id.user_home);
+
+        userInformation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, UserInformationActivity.class));
+            }
+        });
+
+        _toLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
     }
 }
